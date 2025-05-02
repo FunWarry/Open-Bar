49a67d94864096e2432e927e1fa4d3102c1fade6
Mathéo Gevraise <82399666+FunWarry@users.noreply.github.com>
2025-05-02 11:06:18 +0200

    Refactor app component to use external HTML and CSS files for better structure

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,22 +3,11 @@ import {RouterOutlet} from '@angular/router';
 import {NavbarComponent} from './core/components/navbar/navbar.component';
 
 @Component({
-    selector: 'app-root',
-    template: `
-    <app-navbar></app-navbar>
-    <div class="content">
-      <router-outlet></router-outlet>
-    </div>
-  `,
-    styles: [`
-    .content {
-      padding-top: 64px; /* Hauteur de la barre de navigation */
-      min-height: calc(100vh - 64px);
-      background-color: #f5f5f5;
-    }
-  `],
-    imports: [RouterOutlet, NavbarComponent],
-    standalone: true
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  imports: [RouterOutlet, NavbarComponent],
+  standalone: true
 })
 export class AppComponent {
   title = 'Gestion Cocktail';
