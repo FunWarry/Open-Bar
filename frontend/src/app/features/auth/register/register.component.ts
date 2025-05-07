5f29ba1bbd7a4c41b96bc67fe6fe1ab965c858c7
Mathéo Gevraise <82399666+FunWarry@users.noreply.github.com>
2025-05-07 08:41:35 +0200

    Enhance registration form with additional fields for name, surname, and role selection

--- frontend/src/app/features/auth/register/register.component.ts
+++ frontend/src/app/features/auth/register/register.component.ts
@@ -1,36 +1,49 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import {Store} from '@ngrx/store';
-import {MatSnackBar} from '@angular/material/snack-bar';
-import {Router} from '@angular/router';
 import * as AuthActions from '../../../core/store/auth.actions';
-import {MatCardModule} from '@angular/material/card';
-import {MatCardHeader, MatCardTitle, MatCardContent} from '@angular/material/card';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCardContent, MatCardHeader, MatCardModule, MatCardTitle} from '@angular/material/card';
+import {MatError, MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
-import {MatError} from '@angular/material/form-field';
-import { NgIf } from '@angular/common';
+import {MatSelectModule} from '@angular/material/select';
+import {NgFor, NgIf} from '@angular/common';
+
 @Component({
-    selector: 'app-register',
-    templateUrl: './register.component.html',
-    styleUrls: ['./register.component.css'],
-    standalone: true,
-    imports: [MatCardModule, MatCardHeader, MatCardTitle, MatCardContent, MatFormFieldModule, MatInputModule, MatButtonModule, MatError, ReactiveFormsModule, NgIf]
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+  standalone: true,
+  imports: [
+    MatCardModule,
+    MatCardHeader,
+    MatCardTitle,
+    MatCardContent,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatError,
+    MatSelectModule,
+    ReactiveFormsModule,
+    NgIf,
+    NgFor
+  ]
 })
 export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
+  roles = ['ADMIN', 'SERVEUR', 'BARMEN'];
 
   constructor(
     private fb: FormBuilder,
-    private store: Store,
-    private snackBar: MatSnackBar,
-    private router: Router
+    private store: Store
   ) {
     this.registerForm = this.fb.group({
+      nom: ['', Validators.required],
+      prenom: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       username: ['', Validators.required],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      role: ['', Validators.required]
     });
   }
 
