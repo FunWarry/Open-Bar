9a5a9ddc6cc7c2fb5c769a2140a8c22995d578d8
Warry <FunWarry@users.noreply.github.com>
2026-07-25 16:43:42 +0200

    fix(#163): ajout annotation @CrossOrigin sur SetupController

--- backend/src/main/java/com/bar/gestioncocktail/controller/SetupController.java
+++ backend/src/main/java/com/bar/gestioncocktail/controller/SetupController.java
@@ -6,6 +6,7 @@ import com.bar.gestioncocktail.dto.UserResponseDTO;
 import com.bar.gestioncocktail.service.SetupService;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/setup")
+@CrossOrigin(origins = "*")
 public class SetupController {
 
     private final SetupService setupService;
