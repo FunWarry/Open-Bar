dd4c371f7007b7551cd157932acbc9b9baf018b7
Warry <FunWarry@users.noreply.github.com>
2026-07-26 18:55:05 +0200

    fix(karma): add coverage reporter to reporters array so lcov.info is populated in CI

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -23,7 +23,7 @@ module.exports = function (config) {
       subdir: '.',
       reporters: [{ type: 'html' }, { type: 'text-summary' }, { type: 'lcovonly' }],
     },
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress', 'kjhtml', 'coverage'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
