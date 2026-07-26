7e8fe2cb19efc3981322a1b15f7bf2ad34afc585
Warry <FunWarry@users.noreply.github.com>
2026-07-26 13:03:18 +0200

    feat(plan-salle): add table side panel and table fusion modal (Figma screens Vue 2, 3, 4)

---
+++ frontend/src/app/features/plan-salle/components/table-side-panel/table-side-panel.component.ts
@@ -0,0 +1,49 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonButton, IonIcon, IonBadge } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { closeOutline, saveOutline, gitMergeOutline, trashOutline } from 'ionicons/icons';
+import { TableBar } from '../../../../core/models/table.model';
+import { RoleBadgeComponent } from '../../../../core/components/ui/role-badge/role-badge.component';
+import { ActionButtonComponent } from '../../../../core/components/ui/action-button/action-button.component';
+
+@Component({
+  selector: 'app-table-side-panel',
+  standalone: true,
+  imports: [
+    CommonModule, FormsModule,
+    IonButton, IonIcon, IonBadge,
+    RoleBadgeComponent, ActionButtonComponent,
+  ],
+  templateUrl: './table-side-panel.component.html',
+  styleUrls: ['./table-side-panel.component.scss'],
+})
+export class TableSidePanelComponent {
+  @Input() table: TableBar | null = null;
+  @Input() isOpen = false;
+
+  @Output() closePanel = new EventEmitter<void>();
+  @Output() saveTable = new EventEmitter<Partial<TableBar>>();
+  @Output() startFusion = new EventEmitter<TableBar>();
+
+  constructor() {
+    addIcons({ closeOutline, saveOutline, gitMergeOutline, trashOutline });
+  }
+
+  onClose() {
+    this.closePanel.emit();
+  }
+
+  onStartFusion() {
+    if (this.table) {
+      this.startFusion.emit(this.table);
+    }
+  }
+
+  onSave() {
+    if (this.table) {
+      this.saveTable.emit(this.table);
+    }
+  }
+}
