8d33569d1a63465677be9e21e3417877c52dd360
Warry <FunWarry@users.noreply.github.com>
2026-07-28 21:21:14 +0200

    test: add unit tests for TestController and WebSocketController to surpass 80% coverage threshold

--- backend/src/test/java/com/bar/gestioncocktail/controller/PublicCommandeControllerTest.java
+++ backend/src/test/java/com/bar/gestioncocktail/controller/PublicCommandeControllerTest.java
@@ -15,7 +15,6 @@ import org.springframework.http.ResponseEntity;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
