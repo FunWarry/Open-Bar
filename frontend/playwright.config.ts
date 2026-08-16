770a91dbbbf78e07919a9ab219e7a0da00c769b2
Warry <FunWarry@users.noreply.github.com>
2026-08-16 16:45:18 +0200

    ci: update e2e job and playwright webServer configuration

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -24,12 +24,10 @@ export default defineConfig({
       use: { ...devices['Desktop Chrome'] },
     },
   ],
-  webServer: process.env['CI']
-    ? undefined
-    : {
-        command: 'npm start',
-        url: 'http://localhost:4200',
-        reuseExistingServer: true,
-        timeout: 120000,
-      },
+  webServer: {
+    command: 'npm start',
+    url: 'http://localhost:4200',
+    reuseExistingServer: !process.env['CI'],
+    timeout: 120000,
+  },
 });
